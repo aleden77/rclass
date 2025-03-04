69eee9ceccc0428c2d3b2fba5308607c203eeb87
Allison Eden <edenalli@msu.edu>
2025-03-04 14:37:06 -0500

    temperature converter

--- scripts/converters.R
+++ scripts/converters.R
@@ -4,3 +4,7 @@ fahr_to_kelvin <- function(temperature_f) {
   kelvin <- (temperature_f - 32) * (5/9) + 273.15
   return(kelvin)
 }
+kelvin_to_celcius <- function(temp) {
+  celcius <- (temp - 32) * (5/9) - 273.15
+  return(celcius)
+}
