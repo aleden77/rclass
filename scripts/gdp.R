4b5e11793eb504252c3e46805583b0b023df4512
Allison Eden <edenalli@msu.edu>
2025-03-04 14:36:55 -0500

    function calc GPD per country

---
+++ scripts/gdp.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+
+gapminder <- read.csv("data/gapminder_data.csv")
+
+calcGDP <- function( gapminder_data, country = NULL){
+     if ( !is.null (country) ) {
+     gapminder_data <- gapminder_data[gapminder$country == country, ]
+     }
+     gdp <- gapminder_data$pop * gapminder_data$gdpPercap
+    return(gdp)
+}
+
